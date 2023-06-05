94c023060f2eb4ced249282a001d26506fdfbb9e
ecniiiv <mrkvnc.a@gmail.com>
2023-06-05 15:55:35 +0800

    initial commit

---
+++ dist/script.js
@@ -0,0 +1,98 @@
+const numberButtons = document.querySelectorAll("button[data-number]");
+const operatorButtons = document.querySelectorAll("button[data-op]");
+const equalsButton = document.querySelector("button[data-equals]");
+const clearButton = document.querySelector("button[data-clear]");
+const delButton = document.querySelector("button[data-del]");
+
+const currInput = document.querySelector("#curr-input");
+const prevInput = document.querySelector("#prev-input");
+
+let currNum = "";
+let prevNum = "";
+let operator = "";
+let result = "";
+
+numberButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    currNum += btn.dataset.number;
+    display(currInput, currNum);
+  });
+});
+
+operatorButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    operator = btn.dataset.op;
+
+    prevNum = getValueFrom(currNum, prevNum);
+    display(prevInput, `${prevNum} ${operator}`);
+    currNum = clearValue();
+    clearDisplay(currInput);
+    console.log(currNum);
+  });
+});
+
+equalsButton.addEventListener("click", () => {
+  result = operate(currNum, operator, prevNum);
+  prevNum = clearValue();
+  currNum = clearValue();
+  clearDisplay(currInput);
+  clearDisplay(prevInput);
+  display(currInput, result);
+});
+
+clearButton.addEventListener("click", () => {
+  prevNum = clearValue();
+  currNum = clearValue();
+  clearDisplay(currInput);
+  clearDisplay(prevInput);
+});
+
+delButton.addEventListener("click", () => {
+  deleteFrom(currInput);
+  currNum = deleteValueFrom(currNum);
+});
+
+function getValueFrom(target, currentValue) {
+  if (target == "" || target == undefined || target == null) {
+    return currentValue;
+  } else {
+    return target;
+  }
+}
+
+function clearValue() {
+  return "";
+}
+
+function display(target, value) {
+  target.innerText = value;
+}
+
+function clearDisplay(target) {
+  target.innerText = "";
+}
+
+function deleteFrom(targetDisplay) {
+  targetDisplay.innerText = targetDisplay.innerText
+    .split("")
+    .slice(0, -1)
+    .join("");
+
+  console.log(targetDisplay);
+}
+
+function deleteValueFrom(targetValue) {
+  return (targetValue = targetValue.split("").slice(0, -1).join(""));
+}
+
+function operate(number, operator, secondNumber) {
+  if (operator === "+") {
+    return +secondNumber + +number;
+  } else if (operator === "-") {
+    return +secondNumber - +number;
+  } else if (operator === "*") {
+    return +secondNumber * +number;
+  } else if (operator === "/") {
+    return +secondNumber / +number;
+  }
+}
